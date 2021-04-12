a2a810e87d3b5e0f8e2a34ad0d317ca0e7d5ae30
Nabin Pyne <nabinpyne1999@gmail.com>
2021-04-12 23:41:47 +0530

    12/04/21

--- C Programming/P-35_H-a-SumOfDigits.c
+++ C Programming/P-35_H-a-SumOfDigits.c
@@ -4,6 +4,8 @@ int main()
     int a, b, c, d, e, f, g, h, i, j;
     printf("Enter a number: ");
     scanf("%d", &a);
+    //take 5 numbers for total them suppose as we take the amount 12345 =>
+
     b = a % 10;
     c = a / 10;
     d = c % 10;
@@ -13,6 +15,7 @@ int main()
     h = g % 10;
     i = g / 10;
     j = b + d + f + h + i;
+    //output as 1+2+3+4+5= 15
     printf("\nTotal number = %d", j);
     return 0;
 }
